57edd07d421af4f25082a01782a1a61162a46176
simonagorgievska12 <gorgievskasimona2004@hotmail.com>
2024-05-26 15:17:17 +0200

    Call method makeBigStringFrom in main class

--- SILab1Main.java
+++ SILab1Main.java
@@ -13,5 +13,9 @@ public class SILab1Main {
         for (int i = 0; i < n; i++) {
             list.add(sc.next());
         }
+<<<<<<< HEAD
 System.out.println(SILab1_1.filterOddLengthStrings(list));    }
+=======
+System.out.println(SILab1_2.makeBigStringFrom(list));    }
+>>>>>>> 3a0c7a1 (Call method makeBigStringFrom in main class)
 }
